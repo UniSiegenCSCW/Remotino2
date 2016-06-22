3e77cf45dd25755d23e032c119076628625f723b
Leon Rische <leon.rische@student.uni-siegen.de>
2016-06-22 17:05:29 +0200

    Remove unused code

--- app/containers/Pin.js
+++ app/containers/Pin.js
@@ -2,15 +2,9 @@ import Pin from '../components/Pin';
 import * as MicrocontrollerActions from '../actions/microcontroller';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-// import { defaultTo } from 'ramda';
-// import { getNested } from '../utils/utils';
 
 function mapStateToProps(state, ownProps) {
-  // const mapping = state.microcontroller.mapping;
   const pin = ownProps.pin;
-
-  // const name = defaultTo(`Pin ${pin.id}`, getNested(mapping, ['pins', pin.id, 'name']));
-  // const tags = defaultTo([], getNested(mapping, ['pins', pin.id, 'categories']));
   return { pin };
 }
 
