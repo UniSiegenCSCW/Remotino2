01f489877ce3d2898f9519e72042bfa385c6eac1
Leon Rische <leon.rische@student.uni-siegen.de>
2016-08-06 23:51:02 +0200

    Filter out pins with no supported modes and serial pins

--- app/containers/Microcontroller.js
+++ app/containers/Microcontroller.js
@@ -2,10 +2,24 @@ import Microcontroller from '../components/Microcontroller';
 import * as MicrocontrollerActions from '../actions/microcontroller';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { values } from 'ramda';
+import { contains, intersection, values } from 'ramda';
+import { MODE_NAMES } from '../reducers/microcontrollerEnums';
 
 const getVisiblePins = (inPins, filter) => {
   let pins = inPins;
+
+  // Filter out all pins that don't have any supported modes
+  // and all pins that belong to the 'Serial' categorie, e.g. TX1, RX1, etc,
+  // because these might be confusing for the user
+  pins = pins.filter(
+    pin => {
+      const supportedModes = intersection(
+        pin.supportedModes,
+        Object.keys(MODE_NAMES).map(k => parseInt(k, 10))
+      );
+      return supportedModes.length > 1 && !contains('Serial', pin.categories);
+    }
+  );
 
   pins = pins.filter(
     pin => (pin.enabled || filter.showDisabled)
