d1917086cb72e173c876ded5f7c9cb41e9b1de36
Leon Rische <leon.rische@student.uni-siegen.de>
2016-08-15 19:22:43 +0200

    Add number-input to Analog Out

--- app/components/Pin/AnalogOutput.js
+++ app/components/Pin/AnalogOutput.js
@@ -24,7 +24,20 @@ export default class DigitalInput extends Component {
 
     return (
       <div>
-        <p className="nomargin">Value: {value}% ({Math.round(value * 2.55)})</p>
+        <p className="nomargin">
+           Value:&nbsp;
+          <input
+            type="number"
+            name="pwm2"
+            min="0" max="100"
+            value={value}
+            size="3"
+            onChange={(e) => {
+              this.handleChange(e.target.value);
+              write(e.target.value * 2.55); // map from 0..100 to 0..255
+            }}
+          /> % ({Math.round(value * 2.55)})
+        </p>
         <input
           type="range"
           name="pwm"
