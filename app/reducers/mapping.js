ac02ca5bb70135dec06d9b98768f983698821d3a
Leon Rische <leon.rische@student.uni-siegen.de>
2016-10-10 20:09:57 +0200

    Change default locale to english

--- app/reducers/mapping.js
+++ app/reducers/mapping.js
@@ -1,7 +1,7 @@
 import update from 'react/lib/update';
 import { IDENTIFIED_BOARD, CHANGE_LOCALE } from '../actions/microcontroller';
 
-const mapping = (state = { name: 'UNKNOWN DEVICE', locale: 'de' }, action) => {
+const mapping = (state = { name: 'UNKNOWN DEVICE', locale: 'en' }, action) => {
   switch (action.type) {
     case IDENTIFIED_BOARD:
       return update(state, { name: { $set: action.name } });
