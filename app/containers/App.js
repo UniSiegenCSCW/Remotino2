08ce01307ddde7fa7adb378738237c9496ac357d
Leon Rische <leon.rische@student.uni-siegen.de>
2016-10-11 23:46:37 +0200

    Remove unused files

--- app/containers/App.js
+++
@@ -1,9 +0,0 @@
-import React, { PropTypes } from 'react';
-
-const App = props => <div>{props.children}</div>;
-
-App.propTypes = {
-  children: PropTypes.element.isRequired
-};
-
-export default App;
