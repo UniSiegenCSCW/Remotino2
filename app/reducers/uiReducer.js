4520720c5c219dc34ff01a99aadca35665d5d5e2
Nicolas Schmidt <schmidt@techp.de>
2017-03-04 12:01:37 +0100

    replay improvement

--- app/reducers/uiReducer.js
+++ app/reducers/uiReducer.js
@@ -1,5 +1,5 @@
 import update from 'react/lib/update';
-import { CHANGE_LOCALE, VISIBLE_INTERVAL_CHANGED } from '../actions/uiActions';
+import { CHANGE_LOCALE, VISIBLE_INTERVAL_CHANGED, AUTOSCROLLING_CHANGED } from '../actions/uiActions';
 import { timestamp } from '../utils/utils';
 
 function startInterval() {
@@ -9,12 +9,14 @@ function startInterval() {
   return [startDate, endDate];
 }
 
-const ui = (state = { locale: 'en', interval: startInterval() }, action) => {
+const ui = (state = { locale: 'en', interval: startInterval(), autoscroll: true }, action) => {
   switch (action.type) {
     case CHANGE_LOCALE:
       return update(state, { locale: { $set: action.locale } });
     case VISIBLE_INTERVAL_CHANGED:
       return update(state, { interval: { $set: action.interval } });
+    case AUTOSCROLLING_CHANGED:
+      return update(state, { autoscroll: { $set: action.autoscroll } });
     default:
       return state;
   }
